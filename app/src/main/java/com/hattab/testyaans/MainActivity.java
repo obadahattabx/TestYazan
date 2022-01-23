9bccf80bc924ac1019d5d658fc64380f25c0fa1b
hp <yazandoha@hotmail.com>
2022-01-23 14:58:05 +0200

    add activity

--- app/src/main/java/com/hattab/testyaans/MainActivity.java
+++ app/src/main/java/com/hattab/testyaans/MainActivity.java
@@ -2,7 +2,13 @@ package com.hattab.testyaans;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.browse.MediaBrowser;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Scroller;
+
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button b;
+        EditText e;
+        Scroller s;
     }
 }
