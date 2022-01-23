510d1274e21d134cb45a023b2ce72f9e8dae1611
hp <yazandoha@hotmail.com>
2022-01-23 15:05:55 +0200

    add naviga

--- app/src/main/java/com/hattab/testyaans/MainActivity.java
+++ app/src/main/java/com/hattab/testyaans/MainActivity.java
@@ -14,6 +14,7 @@ import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
     NavigationView navigationView;
+    boolean b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
